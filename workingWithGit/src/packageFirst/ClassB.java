dc20066f73fbf1dc7eed502fa9e864329881ea47
diadin <d_flori@yahoo.com>
2020-09-23 15:30:42 +0300

    Master - new class ClassB

---
+++ workingWithGit/src/packageFirst/ClassB.java
@@ -0,0 +1,13 @@
+package packageFirst;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		
+		ClassB  objectB = new ClassB();
+		
+		System.out.println(objectB.getClass().getName());
+
+	}
+
+}
